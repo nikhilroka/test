8d9ebb44ac717b00867d2825d99190dd0ba4caaf
Nikhil Rokka <nikhil.rokka@gmail.com>
2020-08-11 13:56:27 +0545

    First commit

---
+++ test.js
@@ -0,0 +1,9 @@
+var rs = require('readline-sync');
+var aNewNum = 20;
+var aNewNewNum = 20;
+
+console.log(aNewNum + aNewNewNum);
+
+var name = rs.question('what is your name?');
+
+console.log("Your name is " + name);
